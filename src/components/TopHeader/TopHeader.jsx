b95e5d2b6decd43d8176098ee4d2fa3ab8d5ff2f
maker-shihab <frontendmaker99@gmail.com>
2024-02-15 11:26:16 +0600

    add error boundary

---
+++ src/components/TopHeader/TopHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './TopHeader.module.css';
+const TopHeader = () => {
+  return (
+    <div className={styles.topHeader}>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, inventore voluptas. Officiis velit corrupti distinctio quis vero natus, vitae tenetur nihil totam voluptatum minus non nemo suscipit fugit amet dignissimos!</p>
+    </div>
+  );
+};
+
+export default TopHeader;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
