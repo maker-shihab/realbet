d335553284eac2e1d6564cf85283e0f630a2dcb4
maker-shihab <frontendmaker99@gmail.com>
2024-02-16 12:05:30 +0600

    UPdate styles

--- src/components/CTA/Cta.jsx
+++ src/components/CTA/Cta.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
-import brands from '../../images/brands.svg';
-import socal from '../../images/socail.svg';
+import { Link } from 'react-router-dom';
+import facebook from '../../images/icons/facebook.png';
+import fluterwave from '../../images/icons/fluterwave.png';
+import instax from '../../images/icons/insta-x.png';
+import instagram from '../../images/icons/instagram.png';
+import interswitch from '../../images/icons/interswitch.png';
+import linkedin from '../../images/icons/linkedin.png';
+import opay from '../../images/icons/opay.png';
+import palmpay from '../../images/icons/palmpay.png';
+import paystack from '../../images/icons/paystack.png';
+import pintarest from '../../images/icons/pintarest.png';
+import snapchart from '../../images/icons/snapchart.png';
+import teligram from '../../images/icons/teligram.png';
+import tiktok from '../../images/icons/tiktok.png';
+import twitter from '../../images/icons/twitter.png';
+import vimio from '../../images/icons/vimio.png';
+import wpurple from '../../images/icons/w-purple.png';
+import whatsapp from '../../images/icons/whatsapp.png';
+import xpresspay from '../../images/icons/xpresspay.png';
+import youtube from '../../images/icons/youtube.png';
 import styles from './Cta.module.css';
 
 const Cta = () => {
@@ -9,16 +27,105 @@ const Cta = () => {
       <div className="container">
         <div className={styles.wrapper}>
 
-          <div className={styles.brands}>
-            <div className={styles.singleBrand}>
-              <img src={brands} alt="brands" />
-            </div>
-          </div>
+          <ul className={styles.brands}>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={interswitch} alt="brands" />
+              </Link>
+            </li>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={paystack} alt="brands" />
+              </Link>
+            </li>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={xpresspay} alt="brands" />
+              </Link>
+            </li>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={fluterwave} alt="brands" />
+              </Link>
+            </li>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={palmpay} alt="brands" />
+              </Link>
+            </li>
+            <li className={styles.singleBrand}>
+              <Link to={"/"}>
+                <img src={opay} alt="brands" />
+              </Link>
+            </li>
+          </ul>
 
           <div className={styles.socail}>
             <ul>
               <li>
-                <img src={socal} alt="social" />
+                <Link to={"/"}>
+                  <img src={twitter} alt="twitter" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={instagram} alt="instagram" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={facebook} alt="facebook" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={youtube} alt="youtube" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={snapchart} alt="snapchart" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={pintarest} alt="pintarest" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={whatsapp} alt="whatsapp" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={tiktok} alt="tiktok" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={vimio} alt="vimio" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={linkedin} alt="linkedin" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={wpurple} alt="wpurple" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={teligram} alt="teligram" />
+                </Link>
+              </li>
+              <li>
+                <Link to={"/"}>
+                  <img src={instax} alt="instax" />
+                </Link>
               </li>
             </ul>
           </div>
